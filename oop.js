13a98a6470250016aad00a13dcba3a08a3e44292
guyong <jiuyige7070@gmail.com>
2024-08-01 17:29:40 +0800

    Initial commit

--- oop.js
+++ oop.js
@@ -1,292 +1,296 @@
-// 表示一个单向链表
 function LinkedList() {
-    this.head = null
+  this._head = null
+  this._length = 0
+}
+
+// 返回链表第idx个结点的值
+LinkedList.prototype.get = function(idx) {
+  var p = this._head
+  while (idx > 0 && p) {
+    p = p.next
+    idx--
   }
+  if (p) {
+    return p.val
+  } else {
+    return undefined
+  }
+}
 
-  // 返回链表第idx个结点的值
-  LinkedList.prototype.get = function(idx) {
-    if (idx < 0) {
-      return null
-    }
+// 设置链表第idx项的值为val
+LinkedList.prototype.set = function(idx, val) {
+  var p = this._head
+  while (idx > 0 && p) {
+    p = p.next
+    idx--
+  }
+  p.val = val
+}
 
-    var current = this.head
-    var count = 0
+// 在链表末尾新增一个结点，值为val
+LinkedList.prototype.push = function(val) {
+   var node = {
+    val: val,
+    next: null,
+   }
+   this._length++
+   if (this._head == null) {
+    this._head = node
+    return
+   }
+   var p = this._head
+   while (p.next) {
+    p = p.next
+   }
+   p.next = node
+}
 
-    while (current !== null) {
-      if (count === idx) {
-        return current.data
-      }
-      count++
-      current = current.next
-    }
-
+// 返回链表末尾结点的值，并删除末尾结点
+LinkedList.prototype.pop = function() {
+  if (this._head == null) { //一个值
+    return undefined
+  }
+  this._length--
+  if (this._head.next == null) { //两个值
+    var result = this.head.val
+    this._head = null
     return null
   }
+  var p = this._head
+  while (p.next.next) {
+    p = p.next
+  }
+  var result = p.next.val
+  p.next = null
+  return result
+}
 
-  // 设置链表第idx项的值为val
-  LinkedList.prototype.set = function(idx, val) {
-    if (idx < 0) {
-      return false
+// 在链表头部新增一个结点，值为val
+LinkedList.prototype.unshift = function(val) {
+  var node = {
+    val: val,
+    next: this._head
+  }
+  this._length++
+  this._head = node
+}
+
+// 返回链表第一个节点的值，并删除这个节点
+LinkedList.prototype.shift = function() {
+  if (this._head == null) {
+    return undefined
+  }
+  this._length--
+  var result = this._head.val
+  this._head = this._head.next
+  return result
+}
+
+LinkedList.prototype.toArray = function() {
+  var result = []
+  var p = this._head
+  while (p) {
+    result.push(p.val)
+    p = p.next
+  }
+  return result
+}
+
+LinkedList.prototype.toString = function() {
+  return this.toArray().join('->')
+}
+
+Object.defineProperty(LinkedList.prototype, 'length', {
+  get: function() {
+    return this._length
+  }
+})
+
+// 表示一个集合（集合中元素没有序，但不能重复）
+// 构造函数可选的可以传入集合中的初始值，但会被去重后存放
+function MySet(initalValues = []) {
+  this._elements = []
+  for (let item of initalValues) {
+    this.add(item)
+  }
+}
+// 向集合中添加元素
+MySet.prototype.add = function (val) {
+  if (!this._elements.includes(val)) {
+    this._elements.push(val)
+  }
+  return this
+}
+// 从集合中删除item元素
+MySet.prototype.delete = function (item) {
+  let idx = this._elements.indexOf(item)
+  if (idx >= 0) {
+    this._elements.splice(idx, 1)
+    return true
+  }
+  return false
+}
+
+// 获取集合中的元素用 c.size，它是一个getter
+Object.defineProperty(MySet.prototype, 'size', {
+  get: function () {
+    return this._elements.length
+  },
+})
+
+// 清空集合中的所有元素
+MySet.prototype.clear = function () {
+  this._elements = []
+}
+
+// 判断集合中是否存在某元素
+MySet.prototype.has = function (item) {
+  return this._elements.includes(item)
+}
+// 遍历集合中的元素（顺序无所谓）
+MySet.prototype.forEach = function (func) {
+  for (let item of this._elements) {
+    func(item)
+  }
+}
+
+// 表示一个映射
+// 这个映射中，可以把任何值映射到任何值，映射的key不限于字符串
+function MyMap(initPairs = []) {
+  // 用一个数组来表示，偶数项表示key，奇数项表示value.[key, val,key,val]
+  this._pairs = []
+  for (let pair of initPairs) {
+    let key = pair[0]
+    let val = pair[1]
+    this.set(key, val)
+  }
+}
+MyMap.prototype = {
+  constructor: MyMap,
+  // 设置映射中的key所对应的值为val
+  set: function (key, val) {
+    for (let i = 0; i < this._pairs.length; i += 2) {
+      if (this._pairs[i] === key) {
+        this._pairs[i + 1] = val
+        return this
+      }
     }
-
-    var current = this.head
-    var count = 0
-
-    while (current !== null) {
-      if (count === idx) {
-        current.data = val
+    this._pairs.push(key, val)
+    return this
+  },
+  // 获取这个映射中key所对应的val
+  get: function (key) {
+    for (let i = 0; i < this._pairs.length; i += 2) {
+      if (this._pairs[i] === key) {
+        return this._pairs[i + 1]
+      }
+    }
+    return undefined
+  },
+  // 判断这个映射中是否存在这个key的映射
+  has: function (key) {
+    for (let i = 0; i < this._pairs.length; i += 2) {
+      if (this._pairs[i] === key) {
         return true
       }
-      count++
-      current = current.next
     }
-
     return false
-  }
-  
-  // 在链表末尾新增一个结点，值为val
-  LinkedList.prototype.push = function(val) {
-      var newNode = new Node(val) // 创建一个新的节点
-      if (this.head === null) {
-          this.head = newNode // 如果链表为空，将新节点设为头节点
-      } else {
-          var current = this.head
-          while (current.next !== null) {
-              current = current.next // 遍历链表找到最后一个节点
-          }
-          current.next = newNode // 将新节点添加到最后一个节点的 next
+  },
+  // 删除这个映射中key及其映射的值的这一对儿
+  delete: function (key) {
+    for (let i = 0; i < this._pairs.length; i++) {
+      if (this._pairs[i] === key) {
+        this._pairs.splice(i, 2)
+        return true
       }
-  }
-
-  // 返回链表末尾结点的值，并删除末尾结点
-  LinkedList.prototype.pop = function() {
-      if (this.head === null) {
-          return null // 如果链表为空，返回null
-      }
-  
-      // 如果链表只有一个节点，返回值
-      if (this.head.next === null) {
-          var value = this.head.data
-          this.head = null
-          return value
-      }
-  
-      var current = this.head
-      var previous = null
-  
-      // 遍历链表直到倒数第二个节点
-      while (current.next !== null) {
-          previous = current
-          current = current.next
-      }
-  
-      var value = current.data // 记录最后一个节点的值
-      previous.next = null // 删除最后一个节点
-      return value // 返回最后一个节点的值
-  }
-
-  // 在链表头部新增一个结点，值为val
-  LinkedList.prototype.unshift = function(val) {
-    var newNode = new Node(val)
-    newNode.next = this.head
-    this.head = newNode
-  }
-  
-  // 返回链表第一个节点的值，并删除这个节点
-  LinkedList.prototype.shift = function() {
-      if (this.head === null) {
-          return null // 如果链表为空，返回 null
-      }
-  
-      var value = this.head.data // 记录头节点的值
-      this.head = this.head.next // 将头节点指向下一个节点
-      return value // 返回原头节点的值
-  }
-
-  LinkedList.prototype.toArray = function() {
-    
-  }
-
-
-
-  // 表示一个集合（集合中元素没有序，但不能重复）
-  // 构造函数可选的可以传入集合中的初始值，但会被去重后存放
-  function MySet(initalValues = []) {
-    this._items = []
-      for (let val of initalValues) {
-        if (!this._items.includes(val)) {
-          this._items.push(val)
-        }
-  }
-  }
-  // 向集合中添加元素
-  MySet.prototype.add = function(item) {
-      if (!this._items.includes(item)) {
-        this._items.push(item)
-      }
-      return this
-  }
-
-  // 从集合中删除 item 元素
-  MySet.prototype.delete = function(item) {
-     var idx = this._items.indexOf(item)
-     if (idx > 0) {
-        this._items.splice(idx, 1)
-     }
-     return this
-  }
-  
-  // 获取集合中的元素用 c.size，它是一个getter
-  Object.defineProperty(MySet.prototype, 'size',{
-    get: function() {
-        return this._items.length
     }
-  })
+    return false
+  },
+  // 清空这个映射中所有的映射对儿
+  clear: function () {
+    this._pairs = []
+    return this
+  },
+  // 获取这个映射中映射对儿的数量
+  get size() {
+    return this._pairs.length / 2
+  },
+  // 遍历这个映射中所有的映射对儿
+  forEach(iterator) {
+    for (let i = 0; i < this._pairs.length; i++) {
+      iterator(this._pairs[i], this._pairs[i + 1])
+    }
+  },
+}
 
+// 表示一个栈：即后进先出，先进后出
+function Stack() {
+  this._elements = []
+}
+// 向栈中增加元素
+Stack.prototype.push = function (val) {
+  this._elements.push(val)
+}
+// 从栈中取出元素并删除栈顶元素
+Stack.prototype.pop = function () {
+  return this._elements.pop()
+}
+// 查看但不删除栈顶元素
+Stack.prototype.peek = function () {
+  return this._elements[this._elements.length - 1]
+}
 
-  // 清空集合中的所有元素
-  MySet.prototype.clear = function() {
-    this._items = []
+// stack.size 获取栈中元素的数量
+Object.defineProperty(Stack.prototype, 'size', {
+  get: function () {
+    return this._elements.length
+  },
+})
+
+// 表示一个队列：即先进先出，后进后出
+function Queue() {
+  this._head = null
+  this._tail = null
+  this._length = 0
+}
+
+// 向队列中增加元素
+Queue.prototype.add = function (val) {
+  let node = {
+    val: val,
+    next: null,
+  }
+  this._length++
+  if (this._head == null) {
+    this._head = this._tail = node
     return this
   }
-
-  // 判断集合中是否存在某元素
-  MySet.prototype.has = function(item) {
-    return this._items.includes(item)
+  this._tail.next = node
+  this._tail = node
+  return this
+}
+// 从队头取出元素并删除队头元素
+Queue.prototype.pop = function () {
+  if (this._head == null) {
+    return undefined
   }
-
-  // 遍历集合中的元素（顺序无所谓）
-  MySet.prototype.forEach = function(func) {
-    for (var value of this._items) {
-        func(value)
-    }
+  this._length--
+  if (this._head === this._tail) {
+    let result = this._head.val
+    this._head = this._tail = null
+    return result
   }
-
-  var c = new MySet() //初始化一个空集合
-  // c.add(5)
-  // c.add(5)
-  // c.size // 1
-  // c.add(8)
-  // c.size // 2
-  // c.has(5) // true
-  // c.has(999) // false
-  // c.delete(5)
-  // c.size // 1
-  // c.delete(9)
-  // c.size // 1
-
-
-
-
-  // 表示一个映射
-  // 这个映射中，可以把任何值映射到任何值，映射的key不限于字符串
-  function MyMap() {
-    this.items = new Object()
-  }
-
-  MyMap.prototype = {
-    // 设置映射中的key所对应的值为val
-    set: function(key, val) {
-      this.items[key] = val
-    },
-    // 获取这个映射中key所对应的val
-    get: function(key) {
-      return this.items[key]
-    },
-    // 判断这个映射中是否存在这个key的映射
-    has: function(key) {
-      return this.items.hasOwnProperty(key)
-    },
-    // 删除这个映射中key及其映射的值的这一对儿
-    delete: function(key) {
-      if (this.has(key)) {
-        delete this.items[key]
-        return true
-      }
-      return false
-    },
-    // 清空这个映射中所有的映射对儿
-    clear: function() {
-      this.items = {}
-    },
-    // 获取这个映射中映射对儿的数量
-    get size() {
-      return Object.keys(this.items).length
-    },
-    // 遍历这个映射中所有的映射对儿
-    forEach(iterator) {
-      for (var key in this.items) {
-        if (this.items.hasOwnProperty(key)) {
-          iterator(key, this.items[key])
-        }
-      }
-    },
-  }
-
-
-  // 表示一个栈：即后进先出，先进后出
-  function Stack() {
-    this._items = []
-  }
-  // 向栈中增加元素
-  Stack.prototype.push = function(val) {
-    this._items.push(val)
-  }
-  // 从栈中取出元素并删除栈顶元素
-  Stack.prototype.pop = function() {
-    if (this._items.length === 0) {
-      return null
-    }
-    return this._items.pop()
-  }
-  // 查看但不删除栈顶元素
-  Stack.prototype.peek = function() {
-    if (this._items.length === 0) {
-      return null
-    }
-    return this.items[this._items.length - 1]
-  }
-
-  // stack.size 获取栈中元素的数量
-
-  // var stack = new Stack()
-  // stack.in(1)
-  // stack.in(2)
-  // stack.size // 2
-  // stack.in(3)
-  // stack.size // 3
-  // stack.out() // 3
-  // stack.out() // 2
-  // stack.in(5)
-  // stacik.out() // 5 
-
-
-  // 表示一个队列：即先进先出，后进后出
-  function Queue() {
-    this.item = []
-  }
-  
-  // 向队列中增加元素
-  Stack.prototype.add = function(val) {
-    this.items.push(val)
-  }
-  // 从队头取出元素并删除队头元素
-  Stack.prototype.pop = function() {
-    if (this.items.lenghth === 0) {
-      return null
-    }
-    return this.items.shift()
-  }
-  // 查看队头元素（没有查看队尾元素的功能）
-  Stack.prototype.peek = function() {
-    if (this.items.length === 0) {
-      return null
-    }
-    return this.items[0]
-  }
-  // 以及queue.size获取队列的长度
-  Object.defineProperty(Queue.prototype, 'size', {
-    get: function() {
-      return this.items.length
-    }
-  })
+  let result = this._head.val
+  this._head = this._head.next
+  return result
+}
+// 查看队头元素（没有查看队尾元素的功能）
+Queue.prototype.peek = function () {
+  return this._head.val
+}
+// 以及queue.size获取队列的长度
+Object.defineProperty(Queue.prototype, 'size', {
+  get: function () {
+    return this._length
+  },
+})
