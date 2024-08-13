19b678236a731392bcbf584b3eb3fcd1afac6f2f
guyong <jiuyige7070@gmail.com>
2024-08-13 18:15:28 +0800

    Initial commit

--- lodash/jiuyige-lodash.js
+++ lodash/jiuyige-lodash.js
@@ -8,11 +8,16 @@ var jiuyige = function() {
         }
         return result
     }
-    function chunk() {
-
+    function chunk1(array, size = 1) {
+        const result = []
+        for (i = 0; i < array.length; i += size) {
+            const chunk1 = array.length.slice(i, i + szie)
+            result.push(chunk1)
+        }
+        return result
     }
     return {
         compact: compact1,
-        chunk: chunk,
+        chunk: chunk1,
     }
 }()
