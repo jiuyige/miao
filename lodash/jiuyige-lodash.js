cd34a2b252735caad47ed82e3099b85e8243cec7
guyong <jiuyige7070@gmail.com>
2024-08-13 19:09:05 +0800

    Initial commit

--- lodash/jiuyige-lodash.js
+++ lodash/jiuyige-lodash.js
@@ -11,13 +11,29 @@ var jiuyige = function() {
     function chunk1(array, size = 1) {
         const result = []
         for (i = 0; i < array.length; i += size) {
-            const chunk1 = array.length.slice(i, i + szie)
+            const chunk1 = array.length.slice(i, i + size)
             result.push(chunk1)
         }
         return result
     }
+    function fill1(array, value, start = 0, end = array.length) {
+        start = Math.max(0, start)
+        end = Math.min(array.length, end)
+
+        for (let i = start; i < end; i++) {
+            array[i] = value
+        }
+
+        return array
+    }
+
+
+
+
+
     return {
         compact: compact1,
         chunk: chunk1,
+        fill: fill1
     }
 }()
